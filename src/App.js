dde18f201bca8f9f5210a913db09bd114f5962f3
Cody De Arkland <codydearkland@gmail.com>
2022-03-06 22:24:05 -0800

    updating himms demo

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import Header from "./components/header";
 import Login from "./components/login";
 
 function App() {
-  const { qrcode, logoversion } = useFlags();
+  const { qrcode, upperimage, logoversion } = useFlags();
 
   return (
-    <div className="App h-screen grid grid-cols-3 grid-rows-3 bg-ldls bg-cover bg-no-repeat">
-      <header className="App-header grid row-start-1 col-span-3">
-        <Header />      
-      </header>
-      <div className="body grid col-span-3 row-start-2 place-items-center">
-        {logoversion ? (
-          <img src={toggle} className="mx-auto max-h-96" alt="logo" />  
-        ) : (  
-            <img src={himms} className="mx-auto max-h-96" alt="logo" />
-        )}  
+    <div className="App h-screen bg-ldls grid grid-cols-3 grid-rows-3  bg-cover bg-no-repeat">
+    {upperimage ? 
+    <div className="body grid col-span-3 row-start-2 xl:row-start-1  place-items-center">
+      {logoversion ? (
+        <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />
+      ) : (
+        <div className="">
+          <img src={ldlogo} className="App-pulse mx-auto h-56 lg:h-80" alt="logo" />
         </div>
-        <div className="body grid row-start-3 col-span-3 place-items-center">
-        {qrcode ? <QRCode /> : <Login />}
-      </div>
+      )}
     </div>
+    : <div />}
+    <header className="App-header grid row-start-1 xl:row-start-2 col-span-3">
+      <Header />
+    </header>
+    <div className="body grid row-start-3 col-span-3 place-items-center">
+      {qrcode ? <QRCode /> : <Login />}
+    </div>
+  </div>
   );
 }
 
